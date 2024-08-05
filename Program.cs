2255255f060f09013136e336b56608b7649e5b17
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-08-05 14:44:50 +0300

    fix parallel execute

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ class Program
         Test(PLinqExecute);
     }
 
-    private static void Test(Func<int, (int result, TimeSpan time)> func)
+    private static void Test(Func<int, (long result, TimeSpan time)> func)
     {
         var (result1, time1) = func(100_000);
         Console.WriteLine($"100.000 элементов. Результат: {result1}, время выполнения: {time1.Ticks} тиков");
@@ -31,9 +31,9 @@ class Program
         Console.WriteLine();
     }
     
-    private static (int result, TimeSpan time)  ConsistentExecute(int size)
+    private static (long result, TimeSpan time)  ConsistentExecute(int size)
     {
-        var result = 0;
+        long result = 0;
         var sw = new Stopwatch();
         var list = GenerateList(size);
         
@@ -46,29 +46,52 @@ class Program
         return (result, time);
     }
     
-    private static (int result, TimeSpan time)  ParallelExecute(int size)
+    private static (long result, TimeSpan time)  ParallelExecute(int size)
     {
-        var result = 0;
+        long result = 0;
         var sw = new Stopwatch();
         var list = GenerateList(size);
         
         object lockObj = new object();
         
+        var subList1 = list.GetRange(0, size / 4);
+        var subList2 = list.GetRange((size - 1)  / 4, size / 4);
+        var subList3 = list.GetRange((size - 1)  / 2, size / 4);
+        var subList4 = list.GetRange((size - 1)  / 4 * 3, size / 4);
+        
         sw.Start();
-        Parallel.For(0, list.Count, (n) =>
+
+        Action<List<int>> action = (List<int> list) =>
         {
-            lock (lockObj) 
-                result += list[n];
-        });
+            foreach (var el in list)
+            {
+                result += el;
+            }
+        };
+
+        Thread t1 = new Thread(() => action(subList1));
+        Thread t2 = new Thread(() => action(subList2));
+        Thread t3 = new Thread(() => action(subList3));
+        Thread t4 = new Thread(() => action(subList4));
+        
+        t1.Start();
+        t2.Start();
+        t3.Start();
+        t4.Start();
+        
+        t1.Join();
+        t2.Join();
+        t3.Join();
+        t4.Join();
         sw.Stop();
         
         var time = sw.Elapsed;
         return (result, time);
     }
     
-    private static (int result, TimeSpan time)  PLinqExecute(int size)
+    private static (long result, TimeSpan time)  PLinqExecute(int size)
     {
-        var result = 0;
+        long result = 0;
         var sw = new Stopwatch();
         var list = GenerateList(size);
         
